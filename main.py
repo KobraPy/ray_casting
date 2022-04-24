c02cc5329c2ab7bad8e96e061a8eca16749056a2
Dênis Santos <outhomexbox@hotmail.com>
2022-04-24 18:12:41 -0300

    Testing intersection

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Turning a line using a trigonometry charts
 import pygame
 from sys import exit
-from time import sleep
+from teste import line_intersection as inter
 
 
 clock = pygame.time.Clock()
@@ -23,12 +23,14 @@ start = [10,10]
 end = [100,100]
 
 
-def set_0(x: int , y: int)-> tuple:
-    '''Pygame uses a quadrant that dosent exits in a traditional graph,
+def set_0(x:float , y: float, s = ())-> tuple:
+    '''This function sets the point (0,0) to the "s" argument'''
+    '''Pygame uses a quadrant that dosn't exits in a traditional graph,
     to uses trigonometric formulas we have trasform the values'''
-     
-    x = start[0] + x
-    y = start[1] - y 
+    if s == ():
+        s = start
+    x = s[0] + x
+    y = s[1] - y 
     return(round(x),round(y))
 
 
@@ -47,6 +49,8 @@ def set_list_coor(start=None,end=None,angle=5):
         new_x_end = (end[0]* 0.9962 ) - (end[1] * 0.0872)
         new_y_end = (end[0]* 0.0872) + (end[1]* 0.9962)
         end = new_x_end,new_y_end
+        
+        inter((start,set_0(end[0],end[1])),((200,200),(200,-200)))
         lista.extend([start,set_0(end[0],end[1])])
     print(lista.__len__())
    
@@ -55,12 +59,14 @@ def set_list_coor(start=None,end=None,angle=5):
     
 while True:
 
+
     start = pygame.mouse.get_pos()
     coordinates  = set_list_coor(start=start,end=end,angle=5)
 
     #screen.blit must be used before the first draw
     screen.fill("black")
     pygame.draw.lines(screen,"white",closed=False,points=coordinates,width=2)
+    pygame.draw.line(screen, "white", start_pos=(set_0(200,200,(250,250))), end_pos=(set_0(200,-200,(250,250))))
     
    
 
@@ -72,7 +78,7 @@ while True:
     
     pygame.display.update()
 
-    clock.tick()
+    clock.tick(1)
 
     
 
